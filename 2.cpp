3bcaa5f01d5124c54c2317831ec4e221f438b5b9
madebyangel99 <angelitarannisa@gmail.com>
2025-04-24 15:35:54 +0700

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    int *p1, *p2, *p3;
+
+    a = 10;
+    b = 15;
+    p1 = &b;
+    p2 = p1;
+    c = 27;
+    p1 = &c;
+    a = *p1;
+    p3 = &b;
+    *p2 = 8;
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "c = " << c << endl;
+}
